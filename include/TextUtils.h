46f07a4951cd91ba9c10ba00cb39842669e63d2b
Sudhip Nashi <sudhip@nashi.us>
2021-01-06 17:21:12 -0600

    Add zalgo character method
    This method creates a zalgo-fied version of the input string.

--- include/TextUtils.h
+++ include/TextUtils.h
@@ -4,4 +4,6 @@
 +(NSString*)spongeCase:(NSString*)string;
 +(NSString*)randomLetters:(NSUInteger)size;
 +(unichar)getSpongeChar:(unichar)character;
++(NSString*)zalgoText:(NSString*)input craziness:(NSUInteger)craziness;
++(NSString*)zalgoChar:(NSString*)input craziness:(NSUInteger)craziness;
 @end
