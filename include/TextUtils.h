f0465b83b7def9f96da5b390d293a2f297fe677e
Sudhip Nashi <sudhip@nashi.us>
2021-01-07 11:16:49 -0600

    I'm only going to use these in one tweak, no need for it to be in shlib

--- include/TextUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-@interface SunflsksTextUtils : NSObject
-+(NSString*)spongeCase:(NSString*)string;
-+(NSString*)randomLetters:(NSUInteger)size;
-+(unichar)getSpongeChar:(unichar)character;
-+(NSString*)zalgoText:(NSString*)input craziness:(NSUInteger)craziness;
-+(NSString*)zalgoChar:(NSString*)input craziness:(NSUInteger)craziness;
-@end
